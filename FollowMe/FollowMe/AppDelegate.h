e598cf60a9a29c4c784cecf68c40dd783ef494db
王妍 <843668546@qq.com>
2016-03-15 15:01:58 +0800

    1.1

---
+++ FollowMe/FollowMe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FollowMe
+//
+//  Created by SCJY on 16/3/15.
+//  Copyright © 2016年 SCJY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
